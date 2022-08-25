a99d033ca00ff9f9ab82e76f71f9a2be1f88e747
Steve Bister <steve.bister@openstudycollege.com>
2022-08-25 15:46:33 +0100

    feat: updates shopify product query

--- app/models/shopify.server.ts
+++ app/models/shopify.server.ts
@@ -8,14 +8,34 @@ export const getProductsFromCollection = async (slug = '') => {
             query: `
       query collectionBySlug($slug: String) {
         collection(handle:$slug) {
-          products(first:10) {
-            edges {
-              node {
+          products(first:12) {
+              nodes {
                 id
                 title
                 handle
+                featuredImage {
+                    altText
+                    height
+                    width
+                    url
+                }
+                options(first: 10) {
+                    values
+                    name
+                }
+                compareAtPriceRange {
+                    minVariantPrice {
+                        amount
+                        currencyCode
+                    }
+                }
+                priceRange {
+                    minVariantPrice {
+                        amount
+                        currencyCode
+                    }
+                }
               }
-            }
           }
         }
       }`,
