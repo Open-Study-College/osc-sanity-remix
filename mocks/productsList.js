734d01d9ac53edcfcaca28e3a872bfa5bde5d10b
Steve Bister <steve.bister@openstudycollege.com>
2022-08-25 16:16:26 +0100

    feat: adds tests

---
+++ mocks/productsList.js
@@ -0,0 +1,388 @@
+export default [
+    {
+        id: 'gid://shopify/Product/7438726791423',
+        title: 'Sage 50c Computerised Accounts Complete',
+        handle: 'sage-50c-computerised-accounts-complete',
+        featuredImage: {
+            altText: 'Sage 50c Computerised Accounts Complete',
+            height: 941,
+            width: 941,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/Sage-50c-Computerised-Accounts-Complete-product.gif?v=1636416627'
+        },
+        options: [
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '649.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438726758655',
+        title: 'Sage 50c Computerised Accounts Level 1',
+        handle: 'sage-50c-computerised-accounts-level-1',
+        featuredImage: null,
+        options: [
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '239.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438726725887',
+        title: 'Sage 50c Computerised Accounts Level 2',
+        handle: 'sage-50c-computerised-accounts-level-2',
+        featuredImage: null,
+        options: [
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '239.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438726660351',
+        title: 'Sage 50c Computerised Accounts Level 3',
+        handle: 'sage-50c-computerised-accounts-level-3',
+        featuredImage: {
+            altText: null,
+            height: 1202,
+            width: 1202,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/Sage-50c-Computerised-Accounts-Level-3-product.gif?v=1636416629'
+        },
+        options: [
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '259.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438726562047',
+        title: 'CIMA Professional Qualification – Managing Performance E2 Module',
+        handle: 'cima-professional-qualification-managing-performance-e2',
+        featuredImage: {
+            altText: 'Managing Performance E2 Module',
+            height: 941,
+            width: 941,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/CIMA-Managing-Performance-E2-module-product.gif?v=1636416609'
+        },
+        options: [
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '599.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438725251327',
+        title: 'ICB Diploma in Payroll Management Level 3',
+        handle: 'icb-payroll-level-3-2020',
+        featuredImage: {
+            altText: 'study payroll',
+            height: 454,
+            width: 800,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/sage-payroll_50609f78-3eef-43f6-b98b-9506a3f87d24.jpg?v=1636416592'
+        },
+        options: [
+            {
+                values: ['Online', 'Study Pack'],
+                name: 'Study-method'
+            },
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '449.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438723055871',
+        title: 'AAT Level 2, Level 3 and Level 4 Accounting',
+        handle: 'aat-accounting-level-2-3-and-4',
+        featuredImage: {
+            altText: null,
+            height: 660,
+            width: 660,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/AAT037-category-image.jpg?v=1636416561'
+        },
+        options: [
+            {
+                values: ['Course Material', 'Course Material + Exams'],
+                name: 'Format'
+            },
+            {
+                values: ['Study Pack'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '1899.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438722302207',
+        title: 'AAT Level 4 Professional Diploma in Accounting',
+        handle: 'aat-level-4-accounting-diploma',
+        featuredImage: {
+            altText: null,
+            height: 1414,
+            width: 2121,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/aat036.jpg?v=1636416544'
+        },
+        options: [
+            {
+                values: ['Course Material', 'Course Material + Exams'],
+                name: 'Format'
+            },
+            {
+                values: ['Study Pack'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '899.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438722040063',
+        title: 'ACCA Complete (P4 and P5 modules)',
+        handle: 'acca-complete-p4-p5',
+        featuredImage: {
+            altText: 'AAT Advanced Diploma in Accounting Level 3',
+            height: 660,
+            width: 660,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/Finance1_product_a3d92749-dc12-4cc3-8f9e-23bb867ececf.jpg?v=1636416537'
+        },
+        options: [
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            },
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '6299.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438722007295',
+        title: 'ACCA Strategic Professional Essentials package',
+        handle: 'acca-strategic-professional-essentials',
+        featuredImage: {
+            altText: null,
+            height: 1708,
+            width: 2560,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/EXP040-scaled.jpg?v=1636416538'
+        },
+        options: [
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            },
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '1099.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438721974527',
+        title: 'ACCA Fundamentals - Applied Skills',
+        handle: 'acca-fundamentals-applied-skills',
+        featuredImage: {
+            altText: null,
+            height: 1707,
+            width: 2560,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/EXP038-scaled.jpg?v=1636416545'
+        },
+        options: [
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            },
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '2899.0',
+                currencyCode: 'GBP'
+            }
+        }
+    },
+    {
+        id: 'gid://shopify/Product/7438721941759',
+        title: 'ACCA Fundamentals - Applied Knowledge',
+        handle: 'acca-fundamentals-applied-knowledge',
+        featuredImage: {
+            altText: null,
+            height: 1707,
+            width: 2560,
+            url: 'https://cdn.shopify.com/s/files/1/0609/0519/3727/products/EXP037-scaled.jpg?v=1636416545'
+        },
+        options: [
+            {
+                values: ['Course Material'],
+                name: 'Format'
+            },
+            {
+                values: ['Online'],
+                name: 'Study-method'
+            }
+        ],
+        compareAtPriceRange: {
+            minVariantPrice: {
+                amount: '0.0',
+                currencyCode: 'GBP'
+            }
+        },
+        priceRange: {
+            minVariantPrice: {
+                amount: '1099.0',
+                currencyCode: 'GBP'
+            }
+        }
+    }
+];
