ef1b28d2727066f2a288220d2417ecb873dc4e50
Steve Bister <steve.bister@openstudycollege.com>
2022-08-26 09:00:32 +0100

    fix: reduced max-width of cards

--- app/components/collections/ProdutGrid.tsx
+++ app/components/collections/ProdutGrid.tsx
@@ -10,7 +10,7 @@ export default function ProductGrid({ products }: Props) {
     if (products.length === 0) return <p>No products</p>;
 
     return (
-        <SimpleGrid as="ul" minChildWidth="450px" columns={3} spacing={12} className="o-container">
+        <SimpleGrid as="ul" minChildWidth="350px" columns={3} spacing={12} className="o-container">
             {products.map((product: shopifyProduct) => (
                 <ProductCard key={product.id} product={product} />
             ))}
