eb760993e142765505148f5927c8184b14626b03
Steve Bister <steve.bister@openstudycollege.com>
2022-08-31 17:15:51 +0100

    feat: refactor query functions into single sanity client query

--- app/models/sanity.server.ts
+++ app/models/sanity.server.ts
@@ -1,174 +1,30 @@
-import { GraphQLClient } from 'graphql-request';
-import type {
-    GetHomeQuery,
-    SettingsQuery,
-    CollectionQuery,
-    ProductQuery,
-    PageQuery,
-    CollectionBySlugQuery,
-    ProductBySlugQuery,
-    PageBySlugQuery,
-    AssetQuery
-} from '~/graphql/sanity/sanity-generated';
-import getHomeQuery from '~/graphql/sanity/home.query';
-import getSettingsQuery from '~/graphql/sanity/settings.query';
-import { collectionsQuery, productsQuery, pagesQuery } from '~/graphql/sanity/pages.query';
-import {
-    productSlugQuery,
-    collectionSlugQuery,
-    pageSlugQuery,
-    assetQuery
-} from '~/graphql/sanity/helpers.query';
+import type { Params } from 'react-router-dom';
+import { getClient } from '~/lib/sanity/getClient.server';
 
-interface pageArgs {
-    slug: string | undefined;
-    useCdn: boolean;
+interface Args {
+    request: Request;
+    params?: Params;
+    query: string;
 }
 
-const clientUrl = `https://${process.env.SANITY_STUDIO_API_PROJECT_ID}.api.sanity.io/v1/graphql/${process.env.SANITY_STUDIO_API_DATASET}/default`;
-const clientUrlCDN = `https://${process.env.SANITY_STUDIO_API_PROJECT_ID}.apicdn.sanity.io/v1/graphql/${process.env.SANITY_STUDIO_API_DATASET}/default`;
+export default async function getPageData({ request, params, query }: Args) {
+    if (!request) throw new Error('Request must be passed');
 
-// https://www.npmjs.com/package/graphql-request
-const graphcms = new GraphQLClient(clientUrl || '', {
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.SANITY_STUDIO_API_TOKEN}`
-    }
-});
-
-export async function getHome({ useCdn }: { useCdn: boolean }) {
-    if (useCdn) {
-        graphcms.setEndpoint(clientUrlCDN);
-    }
+    const requestUrl = new URL(request?.url);
+    const isPreview = requestUrl?.searchParams?.get('preview') ? true : false;
 
     try {
-        const { allHome } = await graphcms.request<GetHomeQuery>(getHomeQuery);
+        const param = params?.slug ? { slug: params.slug } : {};
 
-        return allHome;
-    } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
-    }
-}
-
-export async function getSettings() {
-    try {
-        const variables = {
-            id: 'settings'
-        };
-        const settings = await graphcms.request<SettingsQuery>(getSettingsQuery, variables);
-
-        return settings;
-    } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
-    }
-}
-
-export async function getCollection({ slug, useCdn }: pageArgs) {
-    if (!slug) console.error('⚠️ Slug is missing or incorrect');
-
-    if (useCdn) {
-        graphcms.setEndpoint(clientUrlCDN);
-    }
-
-    try {
-        const variables = {
-            slug
-        };
+        const querySanityDataset = await getClient(isPreview).fetch(query, param);
 
         // @ts-ignore
-        const { allCollection } = await graphcms.request<CollectionQuery>(
-            collectionsQuery,
-            variables
-        );
+        const pageData = querySanityDataset.filter((item) =>
+            isPreview ? item._id.includes('drafts') : !item._id.includes('drafts')
+        )[0];
 
-        return allCollection;
+        return { page: pageData, isPreview };
     } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
-    }
-}
-
-export async function getProduct({ slug, useCdn }: pageArgs) {
-    if (!slug) console.error('⚠️ Slug is missing or incorrect');
-
-    if (useCdn) {
-        graphcms.setEndpoint(clientUrlCDN);
-    }
-
-    try {
-        const variables = {
-            slug
-        };
-
-        // @ts-ignore
-        const { allProduct } = await graphcms.request<ProductQuery>(productsQuery, variables);
-
-        return allProduct;
-    } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
-    }
-}
-
-export async function getPage({ slug, useCdn }: pageArgs) {
-    if (!slug) console.error('⚠️ Slug is missing or incorrect');
-
-    if (useCdn) {
-        graphcms.setEndpoint(clientUrlCDN);
-    }
-
-    const variables = {
-        slug
-    };
-
-    // @ts-ignore
-    const { allPage } = await graphcms.request<PageQuery>(pagesQuery, variables);
-
-    return allPage;
-}
-
-export async function getInteralUrl(ref: string) {
-    if (!ref) console.error('⚠️ Ref is missing or incorrect');
-
-    try {
-        const variables = {
-            id: ref
-        };
-
-        let query;
-        const isShopifyProduct = ref.includes('shopifyProduct');
-        const isShopifyCollection = ref.includes('shopifyCollection');
-
-        if (isShopifyProduct) {
-            query = await graphcms.request<ProductBySlugQuery>(productSlugQuery, variables);
-        } else if (isShopifyCollection) {
-            query = await graphcms.request<CollectionBySlugQuery>(collectionSlugQuery, variables);
-        } else {
-            query = await graphcms.request<PageBySlugQuery>(pageSlugQuery, variables);
-        }
-
-        return query;
-    } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
-    }
-}
-
-export async function getAsset(ref: string) {
-    if (!ref) console.error('⚠️ Ref is missing or incorrect');
-
-    try {
-        const variables = {
-            id: ref
-        };
-
-        const asset = graphcms.request<AssetQuery>(assetQuery, variables);
-
-        return asset;
-    } catch (err) {
-        console.error(JSON.stringify(err, undefined, 2));
-        process.exit(1);
+        console.error(err);
     }
 }
