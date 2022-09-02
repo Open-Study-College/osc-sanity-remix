d323e5a16964bc87a8cce3f20b6f28f50910c0c6
Steve Bister <steve.bister@openstudycollege.com>
2022-09-02 09:04:43 +0100

    chore: adds some comments around the redirect

--- app/routes/studio/$.tsx
+++ app/routes/studio/$.tsx
@@ -5,6 +5,13 @@ import { redirect } from '@remix-run/server-runtime';
 export async function loader({ params }: LoaderArgs) {
     console.log({ params });
 
+    // Currently with Studio V2 we can't host it as a subdirectory with Remix seamlessly.
+    // When a user navigates to the /studio route then the Studio will load up okay and the
+    // user can access everything.
+    // However if they directly access /studio/desk (for example) or refresh the page then
+    // Remix will take over and throw a 404 error.
+    // Adding this redirect allows us to push the page back to the Sanity Studio startup page
+    // and the catch boundry is there as a safety net.
     return redirect('/studio');
 }
 
