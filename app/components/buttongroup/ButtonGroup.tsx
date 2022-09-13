6f97797b715d0fd07f6aee34b7aca5d4df0a7ef9
Steve Bister <steve.bister@openstudycollege.com>
2022-09-13 17:06:56 +0100

    fix: moved links into smaller components to make typing clearer

--- app/components/buttongroup/ButtonGroup.tsx
+++ app/components/buttongroup/ButtonGroup.tsx
@@ -7,26 +7,46 @@ interface Props {
     links: InternalSanityLinkItem[] | ExternalSanityLinkItem[];
 }
 
+function InternalButton({ link }: { link: InternalSanityLinkItem }) {
+    if ('slug' in link && link.slug) {
+        return (
+            <Button key={link._key} as={Link} to={link.slug} className="u-bg-primary">
+                {link.title}
+            </Button>
+        );
+    }
+
+    return null;
+}
+
+function ExternalButton({ link }: { link: ExternalSanityLinkItem }) {
+    if ('url' in link) {
+        return (
+            <Button
+                key={link._key}
+                as={ChakraLink}
+                href={link.url}
+                isExternal={link.newWindow}
+                rel="noreferrer"
+                className="u-bg-primary"
+            >
+                {link.title}
+                {link.newWindow ? <ExternalLinkIcon mx={2} /> : null}
+            </Button>
+        );
+    }
+
+    return null;
+}
+
 export default function ButtonGroup({ links }: Props) {
     return (
         <ChakraButtonGroup size="lg" spacing={4}>
             {links.map((link) =>
                 link._type === 'linkInternal' ? (
-                    <Button key={link._key} as={Link} to={link.slug} className="u-bg-primary">
-                        {link.title}
-                    </Button>
+                    <InternalButton link={link} />
                 ) : (
-                    <Button
-                        key={link._key}
-                        as={ChakraLink}
-                        href={link.url}
-                        isExternal={link.newWindow}
-                        rel="noreferrer"
-                        className="u-bg-primary"
-                    >
-                        {link.title}
-                        {link.newWindow ? <ExternalLinkIcon mx={2} /> : null}
-                    </Button>
+                    <ExternalButton link={link} />
                 )
             )}
         </ChakraButtonGroup>
